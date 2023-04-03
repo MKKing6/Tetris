16cfaee28ac4504e7e88ca62bad4bf33c8b3d3ea
MKKing6 <69699405+MKKing6@users.noreply.github.com>
2023-04-03 19:53:47 +0000

    Test

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path');
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname, '/templates/index.html'));
 })
 
 app.listen(port, () => {
