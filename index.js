bf63b5c8963159493d29d455be499c9b55f5fde6
MKKing6 <69699405+MKKing6@users.noreply.github.com>
2023-04-04 12:41:05 +0000

    Stuff

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 const path = require('path');
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/templates/index.html'));
---
+++ static/main.js
@@ -0,0 +1,5 @@
+function draw() {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    ctx.strokeRect(0, 0, 30, 30);
+}
