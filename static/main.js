3dc59d4cfa6271e0a0bb8709d45af57a25b89be3
MKKing6 <69699405+MKKing6@users.noreply.github.com>
2023-05-03 20:07:17 +0000

    grid outline

--- static/main.js
+++ static/main.js
@@ -1,10 +1,4 @@
-const height = document.querySelector("#height span");
-const width = document.querySelector("#width span");
-
-function drawGrid() {
-    const canvas = document.getElementById("grid");
-    const ctx = canvas.getContext("2d");
-    var w = window.innerWidth;
+var w = window.innerWidth;
     var h = window.innerHeight;
     if (w < h) {
         var length = w * .7;
@@ -12,11 +6,32 @@ function drawGrid() {
     else {
         var length = h * .7;
     }
+
+function drawLine(ctx, begin, end, stroke = 'black', width = 1) {
+    if (stroke) {
+        ctx.strokeStyle = stroke;
+    }
+
+    if (width) {
+        ctx.lineWidth = width;
+    }
+
+    ctx.beginPath();
+    ctx.moveTo(...begin);
+    ctx.lineTo(...end);
+    ctx.stroke();
+}
+
+function drawGrid() {
+
+    const canvas = document.getElementById("grid");
+    const ctx = canvas.getContext("2d");
     
-    canvas.setAttribute('width', length/2);
-    canvas.setAttribute('height', length);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    canvas.setAttribute('width', w);
+    canvas.setAttribute('height', h);
     ctx.lineWidth = 1;
-    ctx.strokeRect(1, 1, length/2 - 2, length - 2);
+    drawLine(ctx, [w/2-(length/2)/2, h/2-length/2], [w/2-(length/2)/2, h/2+length/2])
     /*for (var i = 0; i < 10; i++) {
         for (var j = 0; j < 20; j++) {
             ctx.strokeRect(i*(length/20), j*(length/20), length/20 - 1, length/20 - 1);
