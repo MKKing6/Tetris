69c4ba9d2bf824ebfdac5ab4e73647ecdd89e3f4
MKKing6 <69699405+MKKing6@users.noreply.github.com>
2023-05-18 12:52:15 +0000

    Tiles

--- static/main.js
+++ static/main.js
@@ -1,9 +1,28 @@
+const tiles = new Image();
+tiles.src = "texture.png";
+const imagePx = 30;
 var boxLength;
-
 var leftGrid;
 var topGrid;
+var grid = [];
 
-function drawLine(ctx, begin, end, stroke = 'black', width = 1) {
+function init() {
+    let arr = [];
+    for (var r = 0; r < 10; r++) {
+        for (var c = 0; c < 40; c++) {
+            arr.push(0);
+        }
+    }
+    while(arr.length) grid.push(arr.splice(0,20));
+    console.log(grid);
+    grid[0][0] = 1;
+}
+
+function drawLine(ctx, begin, end, stroke = "black", opacity = 1, width = 1) {
+    if (opacity) {
+        ctx.globalAlpha = opacity;
+    }
+
     if (stroke) {
         ctx.strokeStyle = stroke;
     }
@@ -27,8 +46,8 @@ function drawGrid() {
     else {
         boxLength = (h * .7)/20;
     }
-    topGrid = h/2-(boxLength*10);
-    leftGrid = w/2-(boxLength*5);
+    topGrid = h/2-((boxLength+1)*10);
+    leftGrid = w/2-((boxLength+1)*5);
 
     const canvas = document.getElementById("grid");
     const ctx = canvas.getContext("2d");
@@ -36,10 +55,23 @@ function drawGrid() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     canvas.setAttribute('width', w);
     canvas.setAttribute('height', h);
-    ctx.lineWidth = 1;
-    for (var r = 1; r <= 10; r++) {
-        drawLine(ctx, [leftGrid+boxLength*r, topGrid], [leftGrid+boxLength*r, topGrid+boxLength*20]);
+    for (var r = 0; r <= 10; r++) {
+        drawLine(ctx, [leftGrid+r*boxLength, topGrid], [leftGrid+r*boxLength, topGrid+20*boxLength], "white", 0.5);
     }
+    for (var c = 0; c <= 20; c++) {
+        drawLine(ctx, [leftGrid, topGrid+c*boxLength], [leftGrid+10*boxLength, topGrid+c*boxLength], "white", 0.5);
+    }
+    for (var r = 0; r < 10; r++) {
+        for (var c = 0; c < 40; c++) {
+            if (grid[r][c] != 0) {
+                console.log(grid[r][c]);
+                //ctx.drawImage(tiles, (imagePx + 1)*(grid[r][c]-1), 0, imagePx, imagePx, leftGrid+r*boxLength, topGrid+c*boxLength, boxLength, boxLength);
+                ctx.drawImage(tiles, (imagePx + 1)*(grid[r][c]-1), 0, imagePx, imagePx, leftGrid+r*boxLength, topGrid+(19-c)*boxLength, boxLength, boxLength);
+            }
+        }
+    }
+    drawLine(ctx, [leftGrid - 2.5, topGrid], [leftGrid - 2.5, topGrid + (boxLength)*20 + 2.5], "white", 1, 5);
+    drawLine(ctx, [leftGrid - 5, topGrid + (boxLength)*20 + 2.5], [leftGrid + (boxLength)*10 + 5, topGrid + (boxLength)*20 + 2.5], "white", 1, 5);
+    drawLine(ctx, [leftGrid + (boxLength)*10 + 2.5, topGrid + (boxLength)*20 + 2.5], [leftGrid + (boxLength)*10 + 2.5, topGrid], "white", 1, 5);
 }
-
 onresize = (event) => drawGrid();
