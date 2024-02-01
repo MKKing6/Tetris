cd17b0ffe41a55d0d805338efc84c60c2347abfa
MKKing6 <69699405+MKKing6@users.noreply.github.com>
2024-02-01 16:05:04 +0000

    Revert "b"
    This reverts commit 9517e16907a3ff94d32e68cb20688dd7cec20086.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, '/')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname, '/templates/index.html'));
 })
 
 app.listen(port, () => {
